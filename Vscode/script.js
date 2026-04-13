90c4b8609a4065d47bcf97e493cbaa3612239bd9
Wender <wenderlucasdev@gmail.com>
2026-04-13 08:49:26 -0300

    initial commit

---
+++ Vscode/script.js
@@ -0,0 +1,23 @@
+function toggleMode(){
+
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    const img = document.querySelector("#profile img")
+    if(html.classList.contains('light')){
+        img.setAttribute("src","assets/avatar-light.png")
+        img.setAttribute("alt","Foto de Mayk Brito usando óculos escuro, jaqueta preta e fundo roxo e rosa")
+        
+    }else {
+        img.setAttribute("src", "assets/avatar.png")
+    }
+
+
+   // if(html.classList.contains('light')) {
+    //    html.classList.remove('light')
+   // } else {
+   //     html.classList.add('light')
+  //  }
+        
+  
+}
